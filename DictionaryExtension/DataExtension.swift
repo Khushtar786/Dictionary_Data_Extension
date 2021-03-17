e4cdd98e28ec65f0ef1779f0afe24e8b4801091d
Khushtar Shaikh <60385112+Khushtar786@users.noreply.github.com>
2021-03-17 18:24:16 +0530

    Add files via upload

---
+++ DictionaryExtension/DataExtension.swift
@@ -0,0 +1,16 @@
+//  DataExtension.swift
+
+
+import Foundation
+
+extension Data {
+    
+    var dictionary: [String: Any]? {
+        do {
+            return try JSONSerialization.jsonObject(with: self, options: []) as? [String: Any]
+        } catch {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
+}
